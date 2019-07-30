1fe81c87502ac4b673fee7ef5e04a61a19b6506f
Louis296 <1zhaozhiwei@gmail.com>
2019-07-30 17:40:03 +0800

    学习了对象的继承和多态

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,9 @@
 class Car {
 	private int size;
 	String name;
+	public Car() {
+		System.out.println("Making a Car...");
+	}
 	
 	public void setsize(int i) {
 		if(i>=10) {
@@ -11,14 +14,17 @@ class Car {
 	public int getsize() {
 		return size;
 	}
-	void belling() {
-		System.out.println("滴滴滴");
-	}
+	
 	int numofcar() {
 		return 3;
 	}
 }
 
+class ElectricCar extends Car{
+	void belling() {
+		System.out.println("  滴滴滴");
+	}
+}
 public class Test{
 	public static void main(String[] args) {
 		Car[] cars;
@@ -26,12 +32,16 @@ public class Test{
 		cars[0]=new Car();
 		cars[0].setsize(10);
 		int size0=cars[0].getsize();
-		cars[0].name="lajitong";
+		cars[0].name="wangzi";
 		System.out.println("我的名字是"+cars[0].name);
 		System.out.println("我的大小是"+size0);
 		int num=cars[0].numofcar();
 		System.out.println("我的弄号是"+num);
-		cars[0].belling();
+		cars[1]=new ElectricCar();
+		cars[1].name="lajitong";
+		System.out.print(cars[1].name);
+		ElectricCar a=(ElectricCar) cars[1];
+		a.belling();
 	}
 	
 }
