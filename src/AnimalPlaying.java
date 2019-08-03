82cb35ad36c95f2885cbe69bc81cc8fae3678220
Louis296 <1zhaozhiwei@gmail.com>
2019-08-03 13:21:27 +0800

    学习类的静态变量及其调用

--- src/AnimalPlaying.java
+++ src/AnimalPlaying.java
@@ -1,17 +1,21 @@
 
 abstract class Animal{
 	private String name;
+	static int AnimalCount=0;
 	public String getname(){
 		return name;
 	}
 	public Animal(String n) {
 		name=n;
+		AnimalCount++;
 	}
 }
 
 class dog extends Animal{
+	static int DogCount=0;
 	public dog(String n) {
 		super(n);
+		DogCount++;
 	}
 	public dog() {
 		this("No name");
@@ -21,6 +25,15 @@ class dog extends Animal{
 	}
 }
 
+class cat extends Animal{
+	public cat(String n) {
+		super(n);
+	}
+	public cat() {
+		this("Null");
+	}
+	
+}
 public class AnimalPlaying {
 
 	public static void main(String[] args) {
@@ -29,5 +42,10 @@ public class AnimalPlaying {
 		d.sayname();
 		dog d1=new dog();
 		d1.sayname();
+		dog d2=new dog();
+		cat c=new cat();
+		
+		System.out.println(Animal.AnimalCount);
+		System.out.println(dog.DogCount);
 	}
 }
