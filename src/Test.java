0be21797b27a1abbb244ec27e2ad8e3e9b1c3722
Louis296 <1zhaozhiwei@gmail.com>
2019-08-07 18:24:04 +0800

    学习静态import

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,11 @@
+import static java.lang.System.out;
 
 class Car {
 	private int size;
 	String name;
 	public static final int MODEL=520;
 	public Car() {
-		System.out.println("Making a Car...");
+		out.println("Making a Car...");
 	}
 	public final void setsize(Integer i) {
 		if(i>=10) {
@@ -19,7 +20,7 @@ class Car {
 
 class ElectricCar extends Car{
 	void belling() {
-		System.out.println("  滴滴滴");
+		out.println("  滴滴滴");
 	}
 }
 
@@ -31,18 +32,18 @@ public class Test{
 		cars[0].setsize(10);
 		int size0=cars[0].getsize();
 		cars[0].name="wangzi";
-		System.out.println("我的名字是"+cars[0].name);
-		System.out.println("我的大小是"+size0);
-		System.out.println("我的型号是"+Car.MODEL);
+		out.println("我的名字是"+cars[0].name);
+		out.println("我的大小是"+size0);
+		out.println("我的型号是"+Car.MODEL);
 		cars[1]=new ElectricCar();
 		cars[1].name="lajitong";
-		System.out.print(cars[1].name);
+		out.print(cars[1].name);
 		ElectricCar a=(ElectricCar) cars[1];
 		a.belling();
 		
 		double d=Double.parseDouble("440.24");
 		boolean b=new Boolean("true").booleanValue();
-		System.out.println(d+" "+b);
+		out.println(d+" "+b);
 	}
 	
 }
