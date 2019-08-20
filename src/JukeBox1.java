c41f68a5b4377cedbb93918ad31bdc8415f9812b
Louis296 <1zhaozhiwei@gmail.com>
2019-08-20 20:42:07 +0800

    学习java文本读取

---
+++ src/JukeBox1.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class JukeBox1 {
+	
+	ArrayList<String> songList=new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new JukeBox1().go();
+	}
+	
+	public void go() {
+		getSongs();
+		System.out.println(songList);
+	}
+	
+	public void getSongs() {
+		try {
+			File file=new File("SongList.txt");
+			BufferedReader reader=new BufferedReader(new FileReader(file));
+			String line=null;
+			while((line=reader.readLine())!=null) {
+				addSong(line);
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void addSong(String lineToParse) {
+		String[] tokens=lineToParse.split("/");
+		songList.add(tokens[0]);
+	}
+
+}
