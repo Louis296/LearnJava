d7621f0bf66c7c0614cb7c8e2d019eddd6706624
Louis296 <1zhaozhiwei@gmail.com>
2019-08-24 23:35:42 +0800

    学习泛型

--- src/JukeBox1.java
+++ src/JukeBox1.java
@@ -1,9 +1,46 @@
 import java.io.*;
 import java.util.*;
 
+class Song implements Comparable<Song>{
+	String title;
+	String artist;
+	String rating;
+	String bpm;
+	
+	public int compareTo(Song s) {
+		return title.compareTo(s.getTitle());
+	}
+	Song (String t,String a,String r,String b){
+		title=t;
+		artist=a;
+		rating=r;
+		bpm=b;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getRating() {
+		return rating;
+	}
+	
+	public String getBpm() {
+		return bpm;
+	}
+	
+	public String toString() {
+		return title;
+	}
+}
+
 public class JukeBox1 {
 	
-	ArrayList<String> songList=new ArrayList<String>();
+	ArrayList<Song> songList=new ArrayList<Song>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -33,7 +70,8 @@ public class JukeBox1 {
 	
 	public void addSong(String lineToParse) {
 		String[] tokens=lineToParse.split("/");
-		songList.add(tokens[0]);
+		Song nextSong=new Song(tokens[0],tokens[1],tokens[2],tokens[3]);
+		songList.add(nextSong);
 	}
 
 }
