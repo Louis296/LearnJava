23f2240244b57aa91813edfcdc2f49f72bbda274
Louis296 <1zhaozhiwei@gmail.com>
2019-08-25 18:10:01 +0800

    学习对象相同性的判断方法

--- src/JukeBox1.java
+++ src/JukeBox1.java
@@ -10,11 +10,21 @@ class Song implements Comparable<Song>{
 	public int compareTo(Song s) {
 		return title.compareTo(s.getTitle());
 	}
+	
 	Song (String t,String a,String r,String b){
 		title=t;
 		artist=a;
 		rating=r;
 		bpm=b;
+	}
+	
+	public boolean equals(Object aSong) {
+		Song s=(Song) aSong;
+		return getTitle().equals(s.getTitle());
+	}
+	
+	public int hashCode() {
+		return title.hashCode();
 	}
 	
 	public String getTitle() {
@@ -47,11 +57,35 @@ public class JukeBox1 {
 		new JukeBox1().go();
 	}
 	
+	class ArtistCompare implements Comparator<Song>{
+		public int compare(Song one,Song two) {
+			return one.getArtist().compareTo(two.getArtist());
+		}
+	}
+	
+	class BpmCompare implements Comparator<Song>{
+		public int compare(Song one,Song two) {
+			return one.getBpm().compareTo(two.getBpm());
+		}
+	}
+	
 	public void go() {
 		getSongs();
 		System.out.println(songList);
 		Collections.sort(songList);
 		System.out.println(songList);
+		
+		ArtistCompare artistCompare=new ArtistCompare();
+		Collections.sort(songList, artistCompare);
+		System.out.println(songList);
+		
+		BpmCompare bpmCompare=new BpmCompare();
+		Collections.sort(songList, bpmCompare);
+		System.out.println(songList);
+		
+		HashSet<Song> songSet=new HashSet<Song>();
+		songSet.addAll(songList);
+		System.out.println(songSet);
 	}
 	
 	public void getSongs() {
